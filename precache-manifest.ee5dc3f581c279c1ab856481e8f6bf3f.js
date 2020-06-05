b7baf07ed0a06c35e41a24ffa8b99e7e9550b86a
Sparco7 <barakbbg@gmail.com>
2020-06-05 17:16:55 +0300

    Updates

---
+++ precache-manifest.ee5dc3f581c279c1ab856481e8f6bf3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698ef430a9ec2f2e84aaec9d0981a417",
+    "url": "/Connect4/index.html"
+  },
+  {
+    "revision": "30b541db502bc9029e0c",
+    "url": "/Connect4/static/css/main.5ee1122c.chunk.css"
+  },
+  {
+    "revision": "b1f7308d2ca66662d251",
+    "url": "/Connect4/static/js/2.2aacc5ff.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/Connect4/static/js/2.2aacc5ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30b541db502bc9029e0c",
+    "url": "/Connect4/static/js/main.ab446f8f.chunk.js"
+  },
+  {
+    "revision": "98e1fa3fd0bc5de63cbd",
+    "url": "/Connect4/static/js/runtime-main.3a51ef8a.js"
+  }
+]);
